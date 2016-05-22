966c8b76e1dbc4f453507c8acb80413e5cb1c024
Santiago Lizardo <santiago.lizardo@gmail.com>
2016-05-22 09:29:03 +0100

    Moving global functions to its own file and declaring a files to autoload

---
+++ tests/GlobalsTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace glm;
+
+class GlobalsTest extends \PHPUnit_Framework_TestCase {
+
+    public function testLookAt() {
+        $a = lookAt(vec3(1,1,1), vec3(2,2,3), vec3(4,4,4));
+        $this->assertNotNull($a);
+    }
+}
+
